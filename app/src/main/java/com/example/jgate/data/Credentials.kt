ac252fcbd98adefe7cecf67e2291346f4500f21e
Dylan O'Halloran <dylangjoseph@Gmail.com>
2026-04-09 21:45:12 +0100

    Credentials file created

--- app/src/main/java/com/example/jgate/data/Credentials.kt
+++ app/src/main/java/com/example/jgate/data/Credentials.kt
@@ -1,2 +1,23 @@
 package com.example.jgate.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Single storeed credential in the Vault of Janus
+ */
+
+@Entity(tableName = "credentials")
+data class Credentials(
+
+    // Assign an unique ID automatically
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val siteName: String,
+    val userName: String,
+    val encryptedPassword: String,
+    val category: String,
+    val notes: String = "",
+    val dateAdded: Long = System.currentTimeMillis(),
+    val dateModified: Long = System.currentTimeMillis()
+)
